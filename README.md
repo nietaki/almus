1238c82153f25b5a5d0966796f2b03cac8641a46
Jacek Królikowski <nietaki@gmail.com>
2018-02-18 21:19:07 +0000

    Spirograph hello world :D

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-created using https://www.npmjs.com/package/p5-manager :
+created using https://www.npmjs.com/package/p5-manager
+
+Run `p5 generate project_name` in root directory to create a project in the collection.
+
+Run `p5 server` to run a server serving all the projects
--- hello_world/sketch.js
+++ hello_world/sketch.js
@@ -1,7 +1,39 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+  midX = windowWidth / 2;
+  midY = windowHeight / 2;
+  console.log(PI);
+  console.log(sin(PI / 2));
+  multi = 50;
+
+  phi = 0;
+  step = 0.02;
+
+  R = 7 * multi;
+  r = 2.4 * multi;
+  d = 3.4 * multi;
 }
 
 function draw() {
+  var c = color('magenta');
+  fill(c);
+  dot(midX, midY);
 
+  x = hypoX(phi, R, r, d);
+  y = hypoY(phi, R, r, d);
+  dot(midX + x, midY + y);
+
+  phi += step;
+}
+
+function dot(x, y) {
+  ellipse(x, y, 8, 8);
+}
+
+function hypoX(phi, R, r, d) {
+  return (R - r) * cos(phi) + d * cos((R - r) * phi / r);
+}
+
+function hypoY(phi, R, r, d) {
+  return (R - r) * sin(phi) - d * sin((R - r) * phi / r);
 }
